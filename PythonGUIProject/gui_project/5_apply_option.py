889544ed6dd3c313065a9462ad1b67b4f7a6633a
JHLT <dev.hoonjo@gmail.com>
2022-01-07 17:10:38 +0900

    220107 first commit

--- PythonGUIProject/gui_project/5_apply_option.py
+++ PythonGUIProject/gui_project/5_apply_option.py
@@ -25,7 +25,7 @@ def del_file():
 
 def browse_dest_path():
     folder_selected = filedialog.askdirectory()
-    if folder_selected is None: # 사용자가 취소를 누를때
+    if folder_selected is '': # 사용자가 취소를 누를때
         return
     txt_dest_path.delete(0, END)
     txt_dest_path.insert(0, folder_selected)
@@ -42,36 +42,81 @@ def start():
         return
 
 def merge_images():
-    images = [Image.open(x) for x in list_file.get(0, END)]
-    # size -> size[0] : width, size[1] : height
-    # widths = [x.size[0] for x in images]
-    # heights = [x.size[1] for x in images]
+    try:
+        img_width = cmb_width.get()
+        if img_width == "원본유지":
+            img_width = -1
+        else:
+            img_width = int(img_width)
 
-    widths, heights = zip(*(x.size for x in images))
+        img_space = cmb_space.get()
+        if img_space == "좁게":
+            img_space = 30
+        elif img_space == "보통":
+            img_space = 60
+        elif img_space == "넓게":
+            img_space = 90
+        else:
+            img_space = 0
 
-
-
-    # 최대 넓이, 전체 높이 구해옴
-    max_width, total_height = max(widths), sum(heights)
-
-    # 스케치북 준비
-    result_img = Image.new('RGB', (max_width, total_height),(255, 255, 255))
-    y_offset = 0
-
-    for idx, img in enumerate(images):
-        result_img.paste(img, (0, y_offset))
-        y_offset += img.size[1]
+        img_format = cmb_format.get().lower()
         
-        progress = (idx + 1) / len(images)
-        print("(idx + 1) / len(images)")
-        print("("+ idx + "+" + 1 + ") / " + len(images))
-        p_var.set(progress)
-        progress_bar.update
 
-    dest_path = os.path.join(txt_dest_path.get(), "Hoon_photo.jpg")
-    result_img.save(dest_path)
-    msgbox.showinfo("알림", "작업이 완료되었습니다.")
+        images = [Image.open(x) for x in list_file.get(0, END)]
+        # size -> size[0] : width, size[1] : height
+        # widths = [x.size[0] for x in images]
+        # heights = [x.size[1] for x in images]
 
+        image_sizes = []
+        if img_width > -1:
+            image_sizes = [(int(img_width), int(img_width * x.size[1] / x.size[0])) for x in images]
+        else:
+            image_sizes = [(x.size[0], x.size[1]) for x in images]
+
+
+
+
+        widths, heights = zip(*(image_sizes))
+
+
+
+        # 최대 넓이, 전체 높이 구해옴
+        max_width, total_height = max(widths), sum(heights)
+
+
+        if img_space > 0:
+            total_height += (img_space * (len(images) - 1))
+
+
+
+
+        # 스케치북 준비
+        result_img = Image.new('RGB', (max_width, total_height),(255, 255, 255))
+        y_offset = 0
+
+        for idx, img in enumerate(images):
+            #width가 원본유지가 아닐때는 이미지 크기 조정
+            if img_width > -1:
+                img = img.resize(image_sizes[idx])
+
+
+            result_img.paste(img, (0, y_offset))
+            y_offset += (img.size[1] + img_space) #height 값 + 사용자가 지정한 간격
+            
+            progress = (idx + 1) / len(images)
+            print("(idx + 1) / len(images)")
+            print("("+ idx + "+" + 1 + ") / " + len(images))
+            p_var.set(progress)
+            progress_bar.update
+
+        #포맷 옵션 처리E
+        file_name = "hoon_photo." + img_format
+
+        dest_path = os.path.join(txt_dest_path.get(), file_name)
+        result_img.save(dest_path)
+        msgbox.showinfo("알림", "작업이 완료되었습니다.")
+    except Exception as err:
+        msgbox.showerror("에러", err)
 
 
  
